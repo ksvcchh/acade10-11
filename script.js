5393fa73df04b621ab204d9273cf56964ffb1f68
Mikita Kasevich <m.kasevich.801@studms.ug.edu.pl>
2024-06-21 21:01:12 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const answerTemplate = "<i>Пример ответа: </i>\"3)\".\n\n\\(1)\\) \\(\\);\n\n\\(2)\\) \\(\\);\n\n\\(3)\\) \\(\\);\n\n\\(4)\\) \\(\\);";
+
+const logicalOr = "\\left[\\begin{array}{l}\n\n\\\\\n\n\\end{array}\\right.";
+
+const logicalAnd = "\\begin{cases}\n\n\\\\\n\n\\end{cases}";
+
+const YesNo = "<i>В ответ введите </i>\"да\"<i> либо </i>\"нет\".";
+
+function copy(arg) {
+    navigator.clipboard.writeText(arg);
+};
+
+function handleCopyAnswerTemplate() {
+    copy(answerTemplate);
+};
+
+function handleCopyLogicalOr() {
+    copy(logicalOr);
+};
+
+function handleCopyLogicalAnd() {
+    copy(logicalAnd);
+};
+
+function handleCopyYesNo() {
+    copy(YesNo);
+};
