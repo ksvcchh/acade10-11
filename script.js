2a7d238ac465645e186482ac60aa7db76a36b33e
Mikita Kasevich <m.kasevich.801@studms.ug.edu.pl>
2024-09-03 17:42:04 +0200

    added some more books

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const answerTemplate = "<i>Пример ответа: </i>\"3)\".\n\n\\(1)\\) \\(\\);\n\n\\(2)\\) \\(\\);\n\n\\(3)\\) \\(\\);\n\n\\(4)\\) \\(\\);";
+const answerTemplate = "<i>Пример ответа: </i>\"3)\".\n\n\\(1)\\) \\(\\);\n\n\\(2)\\) \\(\\);\n\n\\(3)\\) \\(\\).";
 
 const logicalOr = "\\left[\\begin{array}{l}\n\n\\\\\n\n\\end{array}\\right.";
 
