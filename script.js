cfdc19fb41ae8a3bd821da3566a6ddfb70aecdea
Mikita Kasevich <m.kasevich.801@studms.ug.edu.pl>
2024-09-28 10:54:26 +0200

    adds feature of opening books through pasting it's name to the page

--- script.js
+++ script.js
@@ -64,4 +64,101 @@ window.addEventListener('DOMContentLoaded', function() {
     function removeTargetBlank() {
         links.forEach(link => link.removeAttribute('target'));
     }
+    function isValidBook(nodeList, book) {
+        if (book == "ЦТ: Теория. Примеры. Тесты. Ларченко") {
+            for (const elem of nodeList) {
+                if (elem.attributes.href.nodeValue == `./учебники/ЦТ_ Теория. Примеры. Тесты. Ларченко.pdf`) {
+                    elem.click();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        if (book == "Алгебра и элементарные функции. Часть 1 и 2. Е. С. Кочетков") {
+            const part = prompt("Какая часть?")
+            if (part == "1") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра и элементарные функции. Часть 1. Е. С. Кочетков.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == "2") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра и элементарные функции. Часть 2. Е. С. Кочетков.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        if (book == "Алгебра и начала математического анализа. 10 класс. Мордкович") {
+            const part = prompt("Какая часть? ('1', если задания, '2' если теория)")
+            if (part == "1") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра и начала математического анализа. 10 класс. Мордкович 2 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == "2") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра и начала математического анализа. 10 класс. Мордкович 1 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        for (const elem of nodeList) {
+            if (elem.attributes.href.nodeValue == `./учебники/${book}.pdf`) {
+                elem.click();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    this.document.addEventListener("paste", function(event) {
+        const book = event.clipboardData.getData('text/plain').trim();
+        bookSearch(event, book);
+    });
+
+    this.document.addEventListener("dragover", function(event) {
+        event.preventDefault();
+    });
+
+    this.document.addEventListener("drop", function(event) {
+        event.preventDefault();
+        const book = event.dataTransfer.getData('text/plain').trim();
+        bookSearch(event, book);
+    });
+
+    function bookSearch(event, book) {
+        const nodeList = Array.from(document.querySelectorAll("div a"));
+        const notification = document.getElementById('notification');
+        // const book = event.clipboardData.getData('text/plain').trim();
+
+        if (!isValidBook(nodeList, book)) {
+            notification.classList.remove('fade-out');
+            notification.classList.add('show');
+        
+            setTimeout(function() {
+                notification.classList.add('fade-out');
+            }, 2000);
+        
+    
+            setTimeout(function() {
+                notification.classList.remove('show');
+            }, 2500);
+        }    
+    }
 });
